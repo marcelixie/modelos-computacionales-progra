2b1e84b07ee20c931f5af46da19f541204de31be
marcelixie <av.marce@comunidad.unam.mx>
2024-09-03 19:02:32 -0600

    tarea 1

---
+++ ejercicios3sept.py
@@ -0,0 +1,52 @@
+# 1) Create a function that get a integer and calculate the factorial of
+# that number (a non-negative integer)
+#
+# fact = int(input('ingresa un número mayor a 1\n'))
+# Fact = fact
+# while fact>1:
+#     Fact= Fact*(fact-1)
+#     fact = fact-1
+# # if 0<=fact<=1:
+# #     Fact = 1
+#
+# print('El factorial es', Fact)
+
+
+# 2) Create a function that receives 3 numbers and verify if the third number is
+# between the 2 first numbers
+# def checar(a, b, c):
+#     if a<=c<=b:
+#         print('YES')
+#     else : print('NO')
+#
+# checar(1,3,-10)
+
+# 3) Given a string, create 2 functions to count the number of spaces in the string
+# string = (' hola  mi nombre es marce')
+# def espacio(string):
+#     list = string.split()
+#     print('el numero de espacios es:' , len(list) - 1)
+
+# espacio(string)
+
+# def space(string):
+#     contador = 0
+#     for i in range(len(string)):
+#         if string[i] == ' ':
+#           contador += 1
+#         else: continue
+#     print(contador)
+#
+# space(string)
+
+# 4) Given a list, create a function to print the odd numbers in the list
+# lista = [1,2,3,4,5,6,7,8,9, 11.5]
+# def impares(lista):
+#     imp = [ ]
+#     for i in range(len(lista)):
+#         if lista[i] %2 == 1:
+#           imp.append(lista[i])
+#         else: continue
+#     print (imp)
+#
+# impares(lista)
